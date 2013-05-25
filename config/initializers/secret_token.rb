cc0119dd043cd591060cc333f21402dc99801734
Justin Louie <justin@cmn.com>
2013-05-25 10:09:29 +0800

    initial commit of confucius-say

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConfuciusSay::Application.config.secret_key_base = 'd5a8defbbed0fe6e4a772f05329154c105044d699d155a05a0291251b529a12b931ce058baeb9e0ba4f26f2bcc73f20767fa8e239464022ab434e1d495643cec'
