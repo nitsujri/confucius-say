5bfc8efa6f9fa855bff4b70ad2c6a2b525fe3981
Justin Louie <justin@cmn.com>
2013-05-26 17:47:53 +0800

    why won't you work in prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,8 @@ end
 
 before 'deploy:update_code', "deploy:add_host_keys"
 before 'deploy:assets:precompile', 'deploy:cp_database_yml'
+after 'deploy:update_code', 'deploy:setup_solr_data_dir'
+after 'deploy:setup_solr_data_dir', 'solr:start'
 after 'deploy:update_code', 'deploy:cleanup'
 
 namespace :deploy do
@@ -49,6 +51,32 @@ namespace :deploy do
   end
 end
 
+namespace :deploy do
+  task :setup_solr_data_dir do
+    unless remote_file_exists?("#{shared_path}/solr/data")
+      run "mkdir -p #{shared_path}/solr/data"
+    end
+  end
+end
+
+namespace :solr do
+  desc "start solr"
+  task :start, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  end
+  desc "stop solr"
+  task :stop, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  end
+  desc "reindex the whole database"
+  task :reindex, :roles => :app do
+    stop
+    # run "rm -rf #{shared_path}/solr/data"
+    start
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex"
+  end
+end
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
@@ -56,10 +84,10 @@ end
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
