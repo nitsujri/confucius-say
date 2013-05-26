3fd741204a43b2bf769e374f70dad468e8b0fd67
Justin Louie <justin@cmn.com>
2013-05-26 21:47:19 +0800

    trying to get bootstrap to be included

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module ConfuciusSay
     #sass > scss
     config.sass.preferred_syntax = :sass
 
+    config.assets.precompile += %w( bootstrap_and_overrides.css )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
