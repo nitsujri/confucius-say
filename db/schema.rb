accc4351a8d482f47a9729ade6e7bede25d01bcc
Justin Louie <justin@cmn.com>
2013-05-29 09:36:40 +0800

    finished individualized words, awesome shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130528144807) do
+ActiveRecord::Schema.define(version: 20130528160057) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20130528144807) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "english_2"
+    t.boolean  "single_char"
   end
 
 end
--- lib/one_offs/individualize_words.rb
+++ lib/one_offs/individualize_words.rb
@@ -2,10 +2,11 @@ class OneOffs
   class IndividualizeWords
 
     def gather_single_chars
+      check = []
       output = []
       collision_cnt = 0
       char_count = 0
-      Word.all.limit(10000).each do |word|
+      Word.all.each do |word|
         if word.chinese_trad.chars.count != word.chinese_simp.chars.count
           raise ">>>> We have something weird! Trad: #{word.chinese_trad}, Simp: #{word.chinese_simp}"
         end
@@ -13,35 +14,56 @@ class OneOffs
         word.chinese_trad.chars.each_with_index do |c_trad, i|
           c_simp = word.chinese_simp[i]
 
-          unless output.include? [c_trad, c_simp]
-            output << [c_trad, c_simp]
+          unless check.include? [c_trad, c_simp]
+
+            #try to grab the jyutping or pinyin
+            jyut = (word.jyutping.split(/\s/)[i] if word.jyutping.present?) || nil 
+            piny = (word.pinyin.split(/\s/)[i] if word.pinyin.present?) || nil
+
+            check << [c_trad, c_simp]
+            output << {
+              :chinese_trad => c_trad,
+              :chinese_simp => c_simp,
+              :jyutping     => jyut,
+              :pinyin       => piny,
+            }
+            char_count += 1
+
           else
             collision_cnt += 1
           end
 
-          char_count += 1
         end
       end
 
-      ap ">>>> Collision Count: #{collision_cnt}/#{char_count}"
+      ap ">>>> Individual Chars: #{char_count}; Collision Count: #{collision_cnt}"
 
       output
     end
 
     def stuff_into_db(chars)
+      new_cnt = 0
+      old_cnt = 0
       chars.each do |char|
-        if word = Word.find_by(chinese_trad: char[0], chinese_simp: char[1])
+        if word = Word.find_by(chinese_trad: char[:chinese_trad], chinese_simp: char[:chinese_simp])
           ap ">>>> Have #{word.chinese_trad}: #{word.id}"
+          old_cnt += 1
         else
           #add & mark
           word = Word.create!({
-            :chinese_trad => char[0],
-            :chinese_simp => char[1],
-            :single_char => true
+            :chinese_trad => char[:chinese_trad],
+            :chinese_simp => char[:chinese_simp],
+            :jyutping     => char[:jyutping],
+            :pinyin       => char[:pinyin],
+            :single_char  => true
           })
+          new_cnt += 1
+          ap word
           ap ">>>> Created #{word.chinese_trad}: #{word.id}"
         end
       end
+
+      ap ">>>> New Words Added: #{new_cnt}; Already Had: #{old_cnt}"
     end
 
     def run
@@ -49,7 +71,7 @@ class OneOffs
       chars = gather_single_chars
 
       #perform search
-      # stuff_into_db(chars)
+      stuff_into_db(chars)
 
       ''
     end
